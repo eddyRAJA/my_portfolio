1be87b473dfb00e0925356adf151c313b39f987b
eddyRAJA <eddy-rajaonarivelo_student2021@wilder.school>
2022-04-07 11:11:05 +0200

    message registered

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,48 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Form\ContactType;
+use Symfony\Component\Mime\Message;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Mime\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ContactController extends AbstractController
 {
+    private $em;
+    private $contactRepo;
+   
+    public function __construct(EntityManagerInterface $em, ContactRepository $contactRepo)
+    {
+        $this->em = $em;
+        $this->ContactRepository = $contactRepo;
+    }
+
     /**
      * @Route("/contact", name="app_contact")
      */
-    public function index(Request $request, MailerInterface $mailer): Response
+    public function registerMessage(Request $request, EntityManagerInterface $em, MailerInterface $mailer): Response
     {
-        $form = $this->createForm(ContactType::class); //à voir si on a déja des données
-        $contact = $form->handleRequest($request);
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) { 
-            $email = (new TemplatedEmail())
-                ->from($contact->get('email')->getData())
-                ->to('portfolio@monsite.com')
-                ->htmlTemplate('contact/message.html.twig')
-                ->context([
-                    'object' => $contact->get('email')->getData(),
-                    'message' => $contact->get('message')->getData()
-                ]);
-            $mailer->send($email);
+            
+            $em->persist($contact);
+            $em->flush();
 
-            $this->addFlash(
-               'success',
-               'Sent message! '
-            );
+        $email = (new TemplatedEmail())
+            ->from($contact->getEmail('email'))
+            ->to('portfolio@monsite.com')
+            ->htmlTemplate('contact/message.html.twig')
+            ->context([
+                'object' => $contact->getEmail('email'),
+                'message' => $contact->getMessage('message')
+            ]);
+        $mailer->send($email);
+
+        $this->addFlash('success', 'Sent message!');
+        return $this->redirectToRoute('app_home');
         }
-
+        
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-    /*$contact->get('firstname')->getData()
-                    ->$contact->get('lastname')->getData()
-                    ->*/
 }
